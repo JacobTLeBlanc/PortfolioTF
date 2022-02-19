1e333d5fe4ccd9bb86f404bdc62edbf290bacd39
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-02-19 12:23:52 -0400

    Added Route53 zone/record

--- route53.tf
+++ route53.tf
@@ -6,6 +6,7 @@ resource "aws_route53_record" "primary_record" {
   zone_id = aws_route53_zone.primary.id
   name = aws_route53_zone.primary.name
   type = "NS"
+  ttl = 30
 
   records = aws_route53_zone.primary.name_servers
 }
