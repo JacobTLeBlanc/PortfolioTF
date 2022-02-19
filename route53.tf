0248fc06214592a217148af91d3974a2a91106d5
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-02-19 12:33:50 -0400

    Comment out to reset env

--- route53.tf
+++ route53.tf
@@ -1,12 +1,12 @@
-resource "aws_route53_zone" "primary" {
-  name = "jacobtleblanc.portfolio.com"
-}
-
-resource "aws_route53_record" "primary_record" {
-  zone_id = aws_route53_zone.primary.id
-  name = aws_route53_zone.primary.name
-  type = "NS"
-  ttl = 30
-
-  records = aws_route53_zone.primary.name_servers
-}
+//resource "aws_route53_zone" "primary" {
+//  name = "jacobtleblanc.portfolio.com"
+//}
+//
+//resource "aws_route53_record" "primary_record" {
+//  zone_id = aws_route53_zone.primary.id
+//  name = aws_route53_zone.primary.name
+//  type = "NS"
+//  ttl = 30
+//
+//  records = aws_route53_zone.primary.name_servers
+//}
