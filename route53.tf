54705e13d77014dc0e197a4028aeff34d49451be
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-02-19 13:24:39 -0400

    Added Route53 validation

--- route53.tf
+++ route53.tf
@@ -16,11 +16,18 @@ resource "aws_route53_zone" "primary" {
 resource "aws_route53_record" "validation" {
   zone_id = aws_route53_zone.primary.id
 
-  name = aws_acm_certificate.default.domain_validation_options[0].resource_record_name
-  type = aws_acm_certificate.default.domain_validation_options[0].resource_record_type
-  records = [aws_acm_certificate.default.domain_validation_options[0].resource_record_value]
+  for_each = {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
   ttl = "300"
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
 }
 
 resource "aws_acm_certificate_validation" "default" {
