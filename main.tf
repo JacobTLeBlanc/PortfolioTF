582f2019a9076dd4c5e8738c95286ddd301fe245
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-02-18 20:02:46 -0400

    Added amplify role

--- main.tf
+++ main.tf
@@ -6,6 +6,27 @@ data "local_file" "buildspec_local" {
   filename = "${path.module}/buildspec.yml"
 }
 
+data "aws_iam_policy_document" "assume_role_amplify_policy" {
+  statement {
+    sid = "lambdaAssumeRolePolicy"
+
+    actions = ["sts:AssumeRole"]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["amplify.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "amplify_role" {
+  name = "GetReposRole"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_amplify_policy.json
+}
+
 resource "aws_amplify_app" "portfolio" {
   name = "portfolio"
   repository = "https://github.com/JacobTLeBlanc/Portfolio_2.0"
@@ -13,6 +34,7 @@ resource "aws_amplify_app" "portfolio" {
   build_spec = data.local_file.buildspec_local.content
 
   access_token = var.git_token
+  iam_service_role_arn = aws_iam_role.amplify_role.arn
 }
 
 resource "aws_amplify_branch" "master" {
