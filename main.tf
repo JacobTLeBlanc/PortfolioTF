a8421497974ec40a7a603a8f4243494f4d503e65
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-02-18 22:44:02 -0400

    Comment out for testing

--- main.tf
+++ main.tf
@@ -1,67 +1,67 @@
-/**
- * Amplify used to host portfolio
- **/
-
-data "local_file" "buildspec_local" {
-  filename = "${path.module}/buildspec.yml"
-}
-
-data "aws_iam_policy_document" "assume_role_amplify_policy" {
-  statement {
-    sid = "AmplifyAssumeRolePolicy"
-
-    actions = ["sts:AssumeRole"]
-
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["amplify.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "portfolio_policy_document" {
-  statement {
-    sid = "PortfolioPolicy"
-
-    effect = "Allow"
-
-    actions = [
-      "amplify:*"
-    ]
-
-    resources = [
-      "*"
-    ]
-  }
-}
-
-resource "aws_iam_role_policy" "portfolio_role_policy" {
-  policy = data.aws_iam_policy_document.portfolio_policy_document.json
-  role   = aws_iam_role.amplify_role.id
-}
-
-resource "aws_iam_role" "amplify_role" {
-  name = "AmplifyRole"
-
-  assume_role_policy = data.aws_iam_policy_document.assume_role_amplify_policy.json
-}
-
-resource "aws_amplify_app" "portfolio" {
-  name = "portfolio"
-  repository = "https://github.com/JacobTLeBlanc/Portfolio_2.0"
-
-  build_spec = data.local_file.buildspec_local.content
-
-  access_token = var.git_token
-  iam_service_role_arn = aws_iam_role.amplify_role.arn
-}
-
-resource "aws_amplify_branch" "master" {
-  app_id = aws_amplify_app.portfolio.id
-  branch_name = "master"
-
-  framework = "React"
-  stage = "PRODUCTION"
-}
+///**
+// * Amplify used to host portfolio
+// **/
+//
+//data "local_file" "buildspec_local" {
+//  filename = "${path.module}/buildspec.yml"
+//}
+//
+//data "aws_iam_policy_document" "assume_role_amplify_policy" {
+//  statement {
+//    sid = "AmplifyAssumeRolePolicy"
+//
+//    actions = ["sts:AssumeRole"]
+//
+//    effect = "Allow"
+//
+//    principals {
+//      type        = "Service"
+//      identifiers = ["amplify.amazonaws.com"]
+//    }
+//  }
+//}
+//
+//data "aws_iam_policy_document" "portfolio_policy_document" {
+//  statement {
+//    sid = "PortfolioPolicy"
+//
+//    effect = "Allow"
+//
+//    actions = [
+//      "amplify:*"
+//    ]
+//
+//    resources = [
+//      "*"
+//    ]
+//  }
+//}
+//
+//resource "aws_iam_role_policy" "portfolio_role_policy" {
+//  policy = data.aws_iam_policy_document.portfolio_policy_document.json
+//  role   = aws_iam_role.amplify_role.id
+//}
+//
+//resource "aws_iam_role" "amplify_role" {
+//  name = "AmplifyRole"
+//
+//  assume_role_policy = data.aws_iam_policy_document.assume_role_amplify_policy.json
+//}
+//
+//resource "aws_amplify_app" "portfolio" {
+//  name = "portfolio"
+//  repository = "https://github.com/JacobTLeBlanc/Portfolio_2.0"
+//
+//  build_spec = data.local_file.buildspec_local.content
+//
+//  access_token = var.git_token
+//  iam_service_role_arn = aws_iam_role.amplify_role.arn
+//}
+//
+//resource "aws_amplify_branch" "master" {
+//  app_id = aws_amplify_app.portfolio.id
+//  branch_name = "master"
+//
+//  framework = "React"
+//  stage = "PRODUCTION"
+//}
